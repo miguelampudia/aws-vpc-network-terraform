35594144894a7d925c3b02665fe4b1d377d6c580
Miguel Angel <miguel.ampudia@outlook.es>
2023-10-18 23:02:33 -0500

    Update security_group_rules.tf

--- modules/eks_vpc/security_group_rules.tf
+++ modules/eks_vpc/security_group_rules.tf
@@ -1,20 +1,20 @@
-resource "aws_security_group_rule" "ing_ssh_rule" {
-  type              = "ingress"
-  from_port         = 22
-  to_port           = 22
-  protocol          = "TCP"
-  cidr_blocks       = ["0.0.0.0/0"]
-  security_group_id = aws_security_group.default_group.id
-}
+#resource "aws_security_group_rule" "ing_ssh_rule" {
+#  type              = "ingress"
+#  from_port         = 22
+#  to_port           = 22
+#  protocol          = "TCP"
+#  cidr_blocks       = ["0.0.0.0/0"]
+#  security_group_id = aws_security_group.default_group.id
+#}
 
-resource "aws_security_group_rule" "ing_http_rule" {
-  type              = "ingress"
-  from_port         = 80
-  to_port           = 80
-  protocol          = "TCP"
-  cidr_blocks       = ["0.0.0.0/0"]
-  security_group_id = aws_security_group.default_group.id
-}
+#resource "aws_security_group_rule" "ing_http_rule" {
+#  type              = "ingress"
+#  from_port         = 80
+#  to_port           = 80
+#  protocol          = "TCP"
+#  cidr_blocks       = ["0.0.0.0/0"]
+#  security_group_id = aws_security_group.default_group.id
+3}
 
 resource "aws_security_group_rule" "default_egress_rule" {
   type              = "egress"
