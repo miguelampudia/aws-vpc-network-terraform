6c82c18c9209c97f9ba7a115d8be6b96bf0b4088
Miguel Angel <miguel.ampudia@outlook.es>
2023-10-18 22:46:23 -0500

    Update main.tf

--- modules/main.tf
+++ modules/main.tf
@@ -28,7 +28,7 @@ module "eks_cluster" {
 module "eks_node_group" {
   source = "./eks_node_group"
 
-  eks_subnet_ids      = flatten([module.eks_vpc.pub_subnets, module.eks_vpc.priv_subnets])
+  eks_subnet_ids      = flatten([module.eks_vpc.priv_subnets])
   eks_cluster_name    = module.eks_cluster.cluster_name
   node_group_name     = var.node_group_name
   node_role_name      = var.node_role_name
